1efff51d133ae87c7b7793b4a53be8b87da08aed
Fabio Spampinato <spampinabio@gmail.com>
2018-02-19 17:41:27 +0100

    Added a `forAll` plugin

---
+++ src/plugins/forall.js
@@ -0,0 +1,47 @@
+
+//TODO: Publish as `gulp-forall`
+
+/* REQUIRE */
+
+const PluginError = require ( 'plugin-error' ),
+      through = require ( 'through2' );
+
+/* FOR ALL */
+
+function forAll ( plugin ) {
+
+  return function pluginWrapper ( ...args ) {
+
+    let files = [];
+
+    return through.obj ( function ( file, encoding, callback ) {
+
+      files.push ( file );
+
+      callback ();
+
+    }, function ( callback ) {
+
+      files = plugin ( files, ...args ) || files;
+
+      if ( files instanceof PluginError ) {
+
+        callback ( files );
+
+      } else {
+
+        files.forEach ( this.push.bind ( this ) );
+
+        callback ();
+
+      }
+
+    });
+
+  }
+
+}
+
+/* EXPORT */
+
+module.exports = forAll;
