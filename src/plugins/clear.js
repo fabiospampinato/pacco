3c1e94765eea805d062ef69e8b7cd54a3404a486
Fabio Spampinato <spampinabio@gmail.com>
2018-02-22 04:07:40 +0100

    Added a `clear` plugin

---
+++ src/plugins/clear.js
@@ -0,0 +1,18 @@
+
+//TODO: Publish as `gulp-clear`
+
+/* REQUIRE */
+
+const forAll = require ( './forall' );
+
+/* CLEAR */
+
+function clear ( files ) {
+
+  return [];
+
+}
+
+/* EXPORT */
+
+module.exports = forAll ( clear );
