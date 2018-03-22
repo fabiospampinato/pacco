193ff9906f71ea0b3da24ed021114352dc3826a7
Fabio Spampinato <spampinabio@gmail.com>
2018-03-22 15:44:32 +0100

    Removed a trailing comma

--- src/project/defaults/plugins.js
+++ src/project/defaults/plugins.js
@@ -13,7 +13,7 @@ const plugins = {
     enabled: true,
     options: {
       browsers: ['ie >= 10', 'ie_mob >= 10', 'edge >= 13', 'ff >= 30', 'chrome >= 34', 'safari >= 7', 'opera >= 23', 'ios >= 7', 'android >= 4.4', 'bb >= 10'],
-      cascade: true,
+      cascade: true
     }
   },
   babel: {
