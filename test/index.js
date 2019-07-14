800480944a7c572f09c56595f43f899dd8524161
Fabio Spampinato <spampinabio@gmail.com>
2019-07-14 22:02:02 +0200

    Rewritten test suite using “test-diff”

--- test/index.js
+++ test/index.js
@@ -1,181 +1,104 @@
 
-// Generating a custom config for each `test/src/*`
-//   Merging a specific `test/config/*.json` with `test/config/general.json`
-// Building in `test/dist/*`
-// Diffing `test/dist/*` against `test/check/*`
-// Can pass option to pacco via `npm run test -- --test-flag -- --pacco-flag`
-// Options:
-//   --only testName => run only this test
-//   --check         => diff `dist` and `check` directories
-
 /* REQUIRE */
 
 const _ = require ( 'lodash' ),
-      argv = require ( 'yargs' ).argv,
-      chalk = require ( 'chalk' ),
+      {argv} = require ( 'yargs' ),
+      diff = require ( 'test-diff' ),
       execa = require ( 'execa' ),
-      fs = require ( 'fs' ),
-      globby = require ( 'globby' ),
-      mkdirp = require ( 'mkdirp' ),
-      path = require ( 'path' ),
-      rimraf = require ( 'rimraf' );
+      path = require ( 'path' );
 
 /* VARIABLES */
 
-const PACCO_BIN = path.resolve ( __dirname, '..', 'bin', 'index.js' );
-
-/* UTILITIES */
-
-async function getTests () {
-
-  const tests = await globby ( '*', {
-    cwd: path.join ( __dirname, 'src' ),
-    onlyDirectories: true
-  });
-
-  if ( argv.only ) {
-
-    if ( tests.includes ( argv.only ) ) return [argv.only];
-
-    console.error ( chalk.red ( `Test "${chalk.underline ( argv.only )}" not found` ) );
-
-    process.exit ( 1 );
-
-  }
-
-  return tests;
-
-}
-
-function getTestPaths ( test ) {
-
-  return {
-    config: path.join ( __dirname, 'config', `${test}.json` ),
-    src: path.join ( __dirname, 'src', `${test}` ),
-    dist: path.join ( __dirname, 'dist', `${test}` ),
-    check: path.join ( __dirname, 'check', `${test}` )
-  };
-
-}
-
-function getTestConfigGeneral ( test ) {
-
-  const escapeBackslash = filePath => filePath.replace ( /\\/g, '\\\\' ); // Required for properly handling Windows paths
-
-  const {src, dist} = getTestPaths ( test ),
-        configPath = path.join ( __dirname, 'config', 'general.json' ),
-        configStr = fs.readFileSync ( configPath, { encoding: 'utf-8' } ),
-        configStrReplaced = configStr.replace ( '[src]', escapeBackslash ( src ) ).replace ( '[dist]', escapeBackslash ( dist ) ),
-        config = JSON.parse ( configStrReplaced );
-
-  return config;
-
-}
-
-function getTestConfigSpecific ( test ) {
-
-  try {
-
-    const {config: configPath} = getTestPaths ( test ),
-          configStr = fs.readFileSync ( configPath, { encoding: 'utf-8' } ),
-          config = JSON.parse ( configStr );
-
-    return config;
-
-  } catch ( e ) {
-
-    return {};
-
-  }
-
-}
-
-function getTestConfig ( test ) {
-
-  const general = getTestConfigGeneral ( test ),
-        specific = getTestConfigSpecific ( test ),
-        config = _.merge ( general, specific );
-
-  return JSON.stringify ( config );
-
-}
-
-/* TESTING */
-
-async function testTest ( test ) {
-
-  await cleanTest ( test );
-
-  await buildTest ( test );
-
-  if ( argv.check ) {
-    await checkTest ( test );
-  }
-
-}
-
-async function cleanTest ( test ) {
-
-  const {dist} = getTestPaths ( test );
-
-  rimraf.sync ( dist );
-
-}
-
-async function buildTest ( test ) {
-
-  const {dist, check} = getTestPaths ( test ),
-        config = getTestConfig ( test ),
-        build = await execa ( PACCO_BIN, ['build', '--config', config, '--no-notification', '--verbose', '--fresh', ...argv._], { reject: false } );
-
-  mkdirp.sync ( check ); // In case we are checking that nothing gets built
-  mkdirp.sync ( dist ); // In case nothing has been built, but we need the folder for `output.txt` files and for diffing
-
-  if ( !argv.check ) { // The `output.txt` files will interfere with diffing
-
-    console.log ( `${chalk.underline ( test )} built` );
-
-    ['stdout', 'stderr'].forEach ( output => {
-      if ( !build[output] ) return;
-      if ( argv.only ) console.log ( build[output] );
-      const outputPath = path.join ( dist, `${output}.txt` );
-      fs.writeFileSync ( outputPath, build[output] );
-    });
-
-  }
-
-}
-
-async function checkTest ( test ) {
-
-  const {dist, check} = getTestPaths ( test ),
-        diff = await execa ( 'git', ['diff', '--no-index', '--color', dist, check], { reject: false } );
-
-  if ( diff.stdout || diff.stderr ) {
-
-    console.log ( chalk.red ( `${chalk.underline ( test )} failed` ) );
-
-    ['stdout', 'stderr'].forEach ( output => {
-      if ( !diff[output] ) return;
-      console.log ( diff[output] );
-    });
-
-  } else {
-
-    console.log ( chalk.green ( `${chalk.underline ( test )} passed` ) );
-
-  }
-
-}
+const PACCO = path.resolve ( __dirname, '..', 'bin', 'index.js' ),
+      CHECK = path.join ( __dirname, 'check' ),
+      CONFIG = path.join ( __dirname, 'config' ),
+      OUTPUT = path.join ( __dirname, 'dist' ),
+      SOURCE = path.join ( __dirname, 'src' ),
+      GLOB = argv.only ? `${argv.only}{}` : '*';
 
 /* TEST */
 
-async function test () {
+const Test = {
 
-  const tests = await getTests ();
+  config: {
 
-  tests.map ( testTest );
+    getGeneral ( test ) {
 
-}
+      const escapeBackslash = filePath => filePath.replace ( /\\/g, '\\\\' ), // Required for properly handling Windows paths
+            {source, output} = Test.paths.get ( test ),
+            configBase = Test.config.getSpecific ( 'general' ),
+            config = JSON.parse ( JSON.stringify ( configBase ).replace ( '[src]', escapeBackslash ( source ) ).replace ( '[dist]', escapeBackslash ( output ) ) );
 
-test ();
+      return config;
+
+    },
+
+    getSpecific ( test ) {
+
+      try {
+
+        return require ( Test.paths.get ( test ).config );
+
+      } catch ( e ) {
+
+        return {};
+
+      }
+
+    },
+
+    get ( test ) {
+
+      const general = Test.config.getGeneral ( test ),
+            specific = Test.config.getSpecific ( test ),
+            config = _.merge ( general, specific );
+
+      return config;
+
+    }
+
+  },
+
+  paths: {
+
+    get ( test ) {
+
+      return {
+        config: path.join ( CONFIG, `${test}.json` ),
+        source: path.join ( SOURCE, test ),
+        output: path.join ( OUTPUT, test ),
+        check: path.join ( CHECK, test )
+      };
+
+    }
+
+  },
+
+  make ( test ) {
+
+    const config = Test.config.get ( test ),
+          result = execa ( PACCO, ['build', '--config', JSON.stringify ( config ), '--no-notification', '--fresh', '--verbose', ...argv._], { reject: false } );
+
+    return result;
+
+  }
+
+};
+
+/* DIFF */
+
+diff ({
+  verbose: !!argv.verbose,
+  source: {
+    cwd: SOURCE,
+    glob: GLOB
+  },
+  output: {
+    cwd: OUTPUT,
+    make: Test.make
+  },
+  check: {
+    cwd: CHECK,
+    glob: GLOB
+  }
+});
